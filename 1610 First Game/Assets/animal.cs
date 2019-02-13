0de53d96e73d5507ad30c750524b4cd03c3da583
Dane Ericksen <10721110@my.uvu.edu>
2019-02-13 11:12:53 -0700

    learned stuff

---
+++ 1610 First Game/Assets/animal.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animal : MonoBehaviour
+{
+    public string name;
+    public floatData health;
+    public floatData speed;
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
